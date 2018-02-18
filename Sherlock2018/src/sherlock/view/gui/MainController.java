d00c2a960b44d590d220fff312ca212864947928
Aliyah <leahxstevens@gmail.com>
2018-02-18 20:08:37 +0000

    Added Action handlers for each of the Main layout tabs

--- Sherlock2018/src/sherlock/view/gui/MainController.java
+++ Sherlock2018/src/sherlock/view/gui/MainController.java
@@ -1,9 +1,15 @@
 package sherlock.view.gui;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
-public class MainController {
+public class MainController implements Initializable{
 	@FXML
 	private Button overviewButton;
 	@FXML
@@ -12,24 +18,52 @@ public class MainController {
 	private Button comparisonButton;
 	@FXML
 	private Button reportButton;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		overviewButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle( ActionEvent event ) {
+				selectOverviewScene();
+			}
+		});
+		
+		matchGraphButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle( ActionEvent event ) {
+				selectMatchGraphScene();
+			}
+		});
+		
+		comparisonButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle( ActionEvent event ) {
+				selectComparisonScene();
+			}
+		});
+		
+		reportButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle( ActionEvent event ) {
+				selectReportScene();
+			}
+		});
+		
+	}
 	
-	@FXML
 	public void selectOverviewScene() {
 		System.out.println("Select the overview scene");
 	}
 	
-	@FXML
 	public void selectMatchGraphScene() {
 		System.out.println("Select the Match graph scene");
 	}
 	
-	@FXML
 	public void selectReportScene() {
 		System.out.println("Select the report scene");
 	}
 	
-	@FXML
-	public void selectSideComparisonScene() {
+	public void selectComparisonScene() {
 		System.out.println("Select the Side by Side comparison scene");
 	}
 }
