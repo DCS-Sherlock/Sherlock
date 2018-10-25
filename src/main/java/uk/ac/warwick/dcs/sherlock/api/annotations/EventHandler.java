74df9f66d06ac65a3dbc878c6f6b56eec316bac7
James <james.mt13@gmail.com>
2018-10-25 18:28:35 +0100

    fix import

--- src/main/java/uk/ac/warwick/dcs/sherlock/api/annotations/EventHandler.java
+++ src/main/java/uk/ac/warwick/dcs/sherlock/api/annotations/EventHandler.java
@@ -4,6 +4,11 @@ import uk.ac.warwick.dcs.sherlock.api.util.Side;
 
 import java.lang.annotation.*;
 
+/**
+ * Marks a method as an event handler. If the containing class object is registered on the event bus, the method will receive events of the type of its required single parameter.
+ *
+ * Set the side parameter to only receive events when sherlock is running as a server or a client
+ */
 @Documented
 @Retention (RetentionPolicy.RUNTIME)
 @Target (ElementType.METHOD)
--- src/main/java/uk/ac/warwick/dcs/sherlock/api/annotations/ResponseHandler.java
+++ src/main/java/uk/ac/warwick/dcs/sherlock/api/annotations/ResponseHandler.java
@@ -1,9 +1,6 @@
 package uk.ac.warwick.dcs.sherlock.api.annotations;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Documented
 @Retention (RetentionPolicy.RUNTIME)
