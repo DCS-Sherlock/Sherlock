da4fb9effbd362a285fd1fb821502dd5d4089d99
James <james.mt13@gmail.com>
2019-01-13 14:39:52 +0000

    Fix code block storage for ITuple format

--- src/main/java/uk/ac/warwick/dcs/sherlock/engine/storage/base/EntityCodeBlock.java
+++ src/main/java/uk/ac/warwick/dcs/sherlock/engine/storage/base/EntityCodeBlock.java
@@ -2,6 +2,7 @@ package uk.ac.warwick.dcs.sherlock.engine.storage.base;
 
 import uk.ac.warwick.dcs.sherlock.api.common.ICodeBlock;
 import uk.ac.warwick.dcs.sherlock.api.common.ISourceFile;
+import uk.ac.warwick.dcs.sherlock.api.util.ITuple;
 
 import java.io.Serializable;
 import java.util.*;
@@ -21,7 +22,7 @@ public class EntityCodeBlock implements ICodeBlock, Serializable {
 	}
 
 	@Override
-	public List<Integer> getLineNumbers() {
+	public List<ITuple<Integer, Integer>> getLineNumbers() {
 		return null;
 	}
 }
