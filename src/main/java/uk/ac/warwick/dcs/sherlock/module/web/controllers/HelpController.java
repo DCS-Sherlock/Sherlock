45213c8338d75ba57f55d6f1bdb4feaf35262b12
Mayney <w.mayne@warwick.ac.uk>
2019-02-13 17:07:26 +0000

    Added locale support to the help centre

--- src/main/java/uk/ac/warwick/dcs/sherlock/module/web/controllers/HelpController.java
+++ src/main/java/uk/ac/warwick/dcs/sherlock/module/web/controllers/HelpController.java
@@ -6,8 +6,10 @@ import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.RequestContextUtils;
 import uk.ac.warwick.dcs.sherlock.module.web.exceptions.LoadingHelpFailed;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,27 +19,24 @@ import java.util.Properties;
 public class HelpController {
 	public HelpController() { }
 
-	//TODO: Add locale support
 	@RequestMapping ("/help")
-	public String index(Model model) throws LoadingHelpFailed {
-		Resource resource = new ClassPathResource("/help.properties");
-		Properties properties = null;
-		Map<String, String> questions = new HashMap<>();
+	public String index(Model model, HttpServletRequest request) throws LoadingHelpFailed {
+		String locale = RequestContextUtils.getLocale(request).toLanguageTag();
 
+		Properties properties;
 		try {
-			properties = PropertiesLoaderUtils.loadProperties(resource);
-		} catch (IOException e) {
-			throw new LoadingHelpFailed("Loading help.properties file failed.");
+			properties = this.loadProperties(locale);
+		} catch (LoadingHelpFailed e) {
+			properties = this.loadProperties("");
 		}
 
-		if (properties != null) {
-			for (String s : properties.stringPropertyNames()) {
-				if (!s.endsWith("_answer")) {
-					questions.put(
-							properties.getProperty(s, ""),
-							properties.getProperty(s+"_answer", "")
-					);
-				}
+		Map<String, String> questions = new HashMap<>();
+		for (String s : properties.stringPropertyNames()) {
+			if (!s.endsWith("_answer")) {
+				questions.put(
+					properties.getProperty(s, ""),
+					properties.getProperty(s+"_answer", "")
+				);
 			}
 		}
 
@@ -55,4 +54,17 @@ public class HelpController {
 		return "help/privacy";
 	}
 
+	private Properties loadProperties(String locale) throws LoadingHelpFailed {
+		if (locale.length() > 0) {
+			locale = "_" + locale;
+		}
+
+		Resource resource = new ClassPathResource("/help" + locale + ".properties");
+
+		try {
+			return PropertiesLoaderUtils.loadProperties(resource);
+		} catch (IOException e) {
+			throw new LoadingHelpFailed("Loading help.properties file failed.");
+		}
+	}
 }
