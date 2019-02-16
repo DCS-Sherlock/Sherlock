89eb75bf18157e8bfeca507fcb803b7240e54d34
Matthew <matthewfox413@gmail.com>
2019-02-16 16:32:46 +0000

    filled in all report descriptions but they're bad

--- src/main/java/uk/ac/warwick/dcs/sherlock/engine/report/ReportDescriptions.java
+++ src/main/java/uk/ac/warwick/dcs/sherlock/engine/report/ReportDescriptions.java
@@ -9,6 +9,7 @@ public class ReportDescriptions {
 
 	/**
 	 * Method to create a basic description that a ReportGenerator can use in a report.
+	 * TODO: these descriptions aren't great and I think some of the detection types themselves can be removed/changed
 	 *
 	 * @param type The type of plagiarism that this description is needed for.
 	 *
@@ -26,21 +27,21 @@ public class ReportDescriptions {
 			case DATA_TYPE:
 				return "These blocks of code are the same except for the data type that the variables are stored in. This does not make a significant difference on the result.";
 			case EXPR_EQUIV:
-				break;
+				return "The expressions here are equivalent, but expressed in a different way.";
 			case REDUNDANT_ADDED:
-				break;
+				return "The statements here are redundant (i.e. they serve no real purpose); the code is otherwise identical.";
 			case STATEMENT_ORDER:
-				break;
+				return "The order of the statements here is different between the different files, but this does not affect the result of running the program.";
 			case ITERATION_STRUCTURE:
-				break;
+				return "The loops here perform the same actions; they differ only in how they iterate over the loop variables, which is superficial in this case.";
 			case SELECT_STRUCTURE:
-				break;
+				return "The structure of the select statements in each file has been altered slightly, which does not impact the result.";
 			case BODY_REPLACE_CALL:
-				break;
+				return "The code in each program is identical, but where in one file a call to a function has been made, in another file the body of that function has been substituted in place of the call.";
 			case NON_STRUCTURED_STATEMENTS:
-				break;
+				return "Statements have been introduced here in a non-structured manner, but the code is otherwise copied.";
 			case COMBINE_NEW_OLD:
-				break;
+				return "This section of code contains a mixture of copied segments and original segments.";
 		}
 
 		return "";
