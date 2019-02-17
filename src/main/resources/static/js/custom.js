6d12a2e980c7b80d5c963b125fe64c340f974010
Mayney <w.mayne@warwick.ac.uk>
2019-02-17 11:53:09 +0000

    Mostly finished network graphing tools

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -112,16 +112,15 @@ function formSubmitButton() {
 
 function networkGraph() {
     if ($("[data-js='networkArea']").length) {
-        var area = $(this);
+        var includedArea = $("#network-included");
+        var includedTemplate = $("#network-included-template");
+        var includedEmptyTemplate = $("#network-included-template-empty");
 
-        var jsonData = graphData();
-        var container = document.getElementById('network-graph')
-        var included = area.find("#network-included");
-        var excluded = area.find("#network-excluded");
-        var includedTemplate = area.find("#network-included-template");
-        var excludedTemplate = area.find("#network-excluded-template");
+        var excludedArea = $("#network-excluded");
+        var excludedTemplate = $("#network-excluded-template");
+        var excludedEmptyTemplate = $("#network-excluded-template-empty");
 
-        var groupDict = {
+        var colours = {
             0: "#00FF00",
             1: "#8DFF00",
             2: "#8DFF00",
@@ -134,8 +133,10 @@ function networkGraph() {
             9: "#FF5700"
         };
 
-        nodes = new vis.DataSet();
-        edges = new vis.DataSet();
+        var container = document.getElementById('network-graph');
+
+        var nodes = new vis.DataSet();
+        var edges = new vis.DataSet();
 
         var data = {
             nodes: nodes,
@@ -143,6 +144,11 @@ function networkGraph() {
         };
 
         var options = {};
+
+        var network = new vis.Network(container, data, options);
+
+        var json = graphData();
+        var dataURL = "";
 
         function addNode(id, name, colour) {
             try {
@@ -152,49 +158,210 @@ function networkGraph() {
                     color: colour
                 });
             }
-            catch (err) {
-                console.log(err);
-            }
+            catch (err) { }
         }
 
-        function addEdge(id, from, to, colour) {
+        function addEdge(from, to, colour) {
+            var id = Math.min(from, to) + "_" + Math.max(from, to);
+
             try {
                 edges.add({
                     id: id,
-                    from: from,
-                    to: to,
+                    from: Math.min(from, to),
+                    to: Math.max(from, to),
                     color: colour
                 });
             }
-            catch (err) {
-                console.log(err);
-            }
+            catch (err) { }
         }
 
-        for(var i = 0; i < jsonData.length; i++) {
-            var node = jsonData[i];
-            addNode(node.id, node.name, groupDict[node.scoreGroup]);
-        }
-
-        for(var i = 0; i < jsonData.length; i++) {
-            var node = jsonData[i];
-
+        function addMatches(node) {
             for(var j = 0; j < node.matches.length; j++) {
-                var node2 = node.matches[j];
-                addEdge(i+j+node.id+node2.id, node.id, node2.id, groupDict[node2.scoreGroup]);
+                var match = node.matches[j];
+                addEdge(node.id, match.id, colours[match.scoreGroup]);
             }
         }
 
-        network = new vis.Network(container, data, options);
+        function clickEvent() {
+            var edgeDelete = $("[data-js='edgeDelete']");
+            if (network.getSelectedEdges().length == 0) {
+                edgeDelete.prop("disabled", true);
+                edgeDelete.addClass("disabled");
+            } else {
+                edgeDelete.prop("disabled", false);
+                edgeDelete.removeClass("disabled");
+            }
 
+            var nodeMatches = $("[data-js='nodeMatches']");
+            var nodeDelete = $("[data-js='nodeDelete']");
+            if (network.getSelectedNodes().length == 0) {
+                nodeMatches.prop("disabled", true);
+                nodeMatches.addClass("disabled");
+                nodeDelete.prop("disabled", true);
+                nodeDelete.addClass("disabled");
+            } else {
+                nodeMatches.prop("disabled", false);
+                nodeMatches.removeClass("disabled");
+                nodeDelete.prop("disabled", false);
+                nodeDelete.removeClass("disabled");
+            }
+        }
+
+        function submissionMatchesEvent(target) {
+            var result = $.grep(json, function( n, i ) {
+                return n.id == target;
+            });
+
+            if (result.length == 1) {
+                var node = result[0];
+                for(var j = 0; j < node.matches.length; j++) {
+                    var match = node.matches[j];
+                    var matchNode = $.grep(json, function( n, i ) {
+                        return n.id == match.id;
+                    });
+                    if (matchNode.length == 1) {
+                        var node2 = matchNode[0];
+                        addNode(node2.id, node2.name, colours[node2.scoreGroup]);
+                        addMatches(node2);
+                    }
+                }
+            }
+
+            update();
+        }
+
+        function bindEvents() {
+            $("[data-js='submissionAdd']").unbind();
+            $("[data-js='submissionAdd']").click(function () {
+                var input = $(this);
+                var target = input.attr("data-js-target");
+
+                var result = $.grep(json, function( n, i ) {
+                    return n.id == target;
+                });
+
+                if (result.length == 1) {
+                    var node = result[0];
+                    addNode(node.id, node.name, colours[node.scoreGroup]);
+                    addMatches(node);
+                }
+
+                update();
+            });
+
+            $("[data-js='submissionMatches']").unbind();
+            $("[data-js='submissionMatches']").click(function () {
+                var input = $(this);
+                var target = input.attr("data-js-target");
+                submissionMatchesEvent(target);
+            });
+
+            $("[data-js='submissionDelete']").unbind();
+            $("[data-js='submissionDelete']").click(function () {
+                nodes.remove({id: $(this).attr("data-js-target")});
+                update();
+            });
+        }
+
+        function update() {
+            includedArea.html("");
+            excludedArea.html("");
+
+            var visible = 0;
+            var invisible = 0;
+
+            for(var i = 0; i < json.length; i++) {
+                var submission = json[i];
+                var node = nodes.get(submission.id);
+
+                if (node == null) { //Not visible on graph
+                    var copy = excludedTemplate.clone();
+                    copy.find(".js-name").text(submission.name);
+                    copy.find(".js-id").attr("data-js-target", submission.id);
+                    excludedArea.append(copy.html());
+
+                    invisible++;
+                } else { //Visible in graph
+                    var copy = includedTemplate.clone();
+                    copy.find(".js-name").text(submission.name);
+                    copy.find(".js-id").attr("data-js-target", submission.id);
+                    includedArea.append(copy.html());
+
+                    visible++;
+                }
+            }
+
+            if (visible == 0) {
+                includedArea.html(includedEmptyTemplate.html());
+            }
+
+            if (invisible == 0) {
+                excludedArea.html(excludedEmptyTemplate.html());
+            }
+
+            bindEvents();
+            clickEvent();
+        }
+
+        network.on('click', function(properties) {
+            clickEvent();
+        });
+
+        network.on("afterDrawing", function (ctx) {
+            dataURL = ctx.canvas.toDataURL();
+            // document.getElementById('image').src = dataURL;
+            // $("a[data-js='downloadGraph']").attr("href", "test");
+        });
+
+
+        $("[data-js='edgeDelete']").click(function () {
+            $(this).blur();
+
+            var selectedEdges = network.getSelectedEdges();
+            for (var i in selectedEdges) {
+                edges.remove(selectedEdges[i]);
+            }
+
+            update();
+        });
+
+        $("[data-js='nodeDelete']").click(function () {
+            $(this).blur();
+
+            var selectedNodes = network.getSelectedNodes();
+            for (var i in selectedNodes) {
+                nodes.remove(selectedNodes[i]);
+            }
+
+            update();
+        });
+
+        $("[data-js='nodeMatches']").click(function () {
+            $(this).blur();
+
+            var selectedNodes = network.getSelectedNodes();
+            for (var i in selectedNodes) {
+                submissionMatchesEvent(selectedNodes[i]);
+            }
+        });
+
+        $("[data-js='graphDownload']").click(function () {
+            var input = $(this);
+            input.blur();
+            input.attr("href", dataURL);
+        });
+
+        update();
+
+        //todo: add download link -> save the canvas as image
+        //https://stackoverflow.com/questions/42663203/export-visjs-network-to-jpeg-png-image
+        //todo: instructions -> click to drag + scroll wheel
     }
 }
 
 function formSubmit(){
     $("[data-js='form']").unbind();
     $("[data-js='form']").submit(function () {
-        console.log("submitting form");
-
         var input = $(this);
         var url = input.attr("action");
         var target = $(input.attr("data-js-target"));
