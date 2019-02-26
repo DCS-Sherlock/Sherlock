17db5a1621e1922b685c1d65ae8e5383a895162c
Matthew <matthewfox413@gmail.com>
2019-02-26 15:14:24 +0000

    Extra DetectionType stuff (all in comments for now)

--- src/main/java/uk/ac/warwick/dcs/sherlock/api/model/detection/DetectionType.java
+++ src/main/java/uk/ac/warwick/dcs/sherlock/api/model/detection/DetectionType.java
@@ -16,10 +16,18 @@ public enum DetectionType {COMMENT, IDENTIFIER, OPERAND_ORDER, DATA_TYPE, EXPR_E
 this is to replace the current version of DetectionType and let users add their own reasons somehow.
 I won't make it final until everything is in place
 
+Now instead of storing a DetectionType enum, store a string which is the name of the reason?
+alternative: DetectionType is a single reason, and theres another class to store all of them?
+*/
+/*
 public class DetectionType {
-	private Map<String, ITuple<String, Float>> reasons;
+	/**
+	 * A map containing each possible justification. The key is a string for the name of that reasons.
+	 * The value is a tuple containing a description of that reason, and a float (0 to 1) with the weighting for it.
+	 *
+	private static Map<String, ITuple<String, Float>> reasons;
 
-	public DetectionType2(List<String> extraReasonNames, List<String> extraReasonDescriptions, List<Float> extraReasonWeights) {
+	public DetectionType(List<String> extraReasonNames, List<String> extraReasonDescriptions, List<Float> extraReasonWeights) {
 		reasons = new HashMap<>();
 
 		//add default reasons
@@ -33,6 +41,30 @@ public class DetectionType {
 		for(int i = 0; i < extraReasonNames.size(); i++) {
 			reasons.put(extraReasonNames.get(i), new Tuple<>(extraReasonDescriptions.get(i), extraReasonWeights.get(i)));
 		}
+	}
+
+	/**
+	 * @return the full map of reasons, including every kind of plagiarism type, including name, description and score weighting.
+	 *
+	public static Map<String, ITuple<String, Float>> getReasons() {
+		return reasons;
+	}
+
+	 /**
+	 * @return a set of every detection type name.
+	 *
+	public static Set<String> getReasonNames() {
+		return reasons.keySet();
+	}
+
+	/**
+	 * Given a type of plagiarism, returns the information relevant to it.
+	 *
+	 * @param name The name of the type of plagiarism to get information for.
+	 * @return The tuple containing the description and score weighting of the specified plagiarism type.
+	 *
+	public static ITuple<String, Float> getReasonInfo(String name) {
+		return reasons.get(name);
 	}
 }
 */
