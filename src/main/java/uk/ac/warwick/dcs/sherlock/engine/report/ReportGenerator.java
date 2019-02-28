d3fcb8705bbfcc4bad75565186a19907cccff215
Matthew <matthewfox413@gmail.com>
2019-02-28 13:19:27 +0000

    Updated report stuff for detection types, general generator improvements

--- src/main/java/uk/ac/warwick/dcs/sherlock/engine/report/ReportGenerator.java
+++ src/main/java/uk/ac/warwick/dcs/sherlock/engine/report/ReportGenerator.java
@@ -5,7 +5,6 @@ import uk.ac.warwick.dcs.sherlock.api.common.ICodeBlockGroup;
 import uk.ac.warwick.dcs.sherlock.api.common.ISourceFile;
 import uk.ac.warwick.dcs.sherlock.api.exception.UnknownDetectionTypeException;
 import uk.ac.warwick.dcs.sherlock.api.model.detection.DetectionType;
-import uk.ac.warwick.dcs.sherlock.api.model.detection.LegecyDetectionType;
 import uk.ac.warwick.dcs.sherlock.api.util.ITuple;
 
 import java.util.*;
@@ -19,12 +18,12 @@ public class ReportGenerator implements IReportGenerator {
 	public FileReport GenerateReport(ISourceFile sourceFile, List<? extends ICodeBlockGroup> codeBlockGroups, List<String> variableNames) {
 		FileReport fileReport = new FileReport(sourceFile.getPersistentId());
 
-		StringJoiner stringJoiner = new StringJoiner("", "", ".");
+		StringJoiner stringJoiner = new StringJoiner("; ", "", ".");
 
 		//Each codeBlockGroup represents a different similarity; each gets its own string added to the report.
 		for (ICodeBlockGroup codeBlockGroup : codeBlockGroups) {
 
-			//New code to get decection type, not connected to rest of this method so the code still compiles
+			//New code to get detection type, not connected to rest of this method so the code still compiles
 			DetectionType detectionType = null;
 			try {
 				detectionType = codeBlockGroup.getDetectionType();
@@ -33,48 +32,43 @@ public class ReportGenerator implements IReportGenerator {
 				e.printStackTrace();
 			}
 
-			LegecyDetectionType legacyDetectionType = null;
+			//Get the base description for this type of plagiarism; it is the first part of the description for this group.
+			stringJoiner.add(detectionType.getReason());
 
-			//Get the base description for this type of plagiarism
-			List<String> descriptionSegments = new ArrayList<>();
-			descriptionSegments.add(ReportDescriptions.getLocationDescription(legacyDetectionType, false));
-
-			//Extend the description for the number of files in the group.
-			for (int i = 1; i < codeBlockGroup.getCodeBlocks().size(); i++) {
-				descriptionSegments.add(ReportDescriptions.getLocationDescription(legacyDetectionType, true));
-			}
-
-			/*
-			 * Get the line numbers and file names for these code blocks and format the description using them.
-			 * Only the first tuple is taken from getLineNumbers.
-			 */
+			//Store a list of file names and line numbers to format the description strings with.
 			//TODO: Ensure distinct file names (add persistent ID?)
 			List<String> fileNames = new ArrayList<>();
-			List<ITuple<Integer, Integer>> lineNumbers = new ArrayList<>();
+			List<List<ITuple<Integer, Integer>>> lineNumbers = new ArrayList<>();
 			for (ICodeBlock codeBlock : codeBlockGroup.getCodeBlocks()) {
-				lineNumbers.add(codeBlock.getLineNumbers().get(0));
+				lineNumbers.add(codeBlock.getLineNumbers());
 				fileNames.add(codeBlock.getFile().getFileDisplayName());
 			}
 
-			//Format each segment of the description with the appropriate file name and line numbers and add to the StringJoiner along the way.
-			//TODO: other formatting including variables, score etc.
+			String filename_string = "File %s: ";
+			//Extend the description for the number of files in the group, and format each string along the way.
+			//TODO: other formatting?
 			for (int i = 0; i < codeBlockGroup.getCodeBlocks().size(); i++) {
-				String formattedString;
-				if (legacyDetectionType == legacyDetectionType.IDENTIFIER) {
-					formattedString = String.format(descriptionSegments.get(i), fileNames.get(i), lineNumbers.get(i).getKey());
-				}
-				else {
-					formattedString = String.format(descriptionSegments.get(i), fileNames.get(i), lineNumbers.get(i).getKey(), lineNumbers.get(i).getValue());
-				}
-				stringJoiner.add(formattedString);
-			}
+				String formatted_filename = String.format(filename_string, fileNames.get(i));
 
-			//Now add the actual description content according to the type of plagiarism in this CodeBlockGroup.
-			try {
-				stringJoiner.add(codeBlockGroup.getDetectionType().getReason()); // NEW
-			}
-			catch (UnknownDetectionTypeException e) {
-				e.printStackTrace();
+				String formatted_lines = "";
+				for(int j = 0; j < lineNumbers.get(i).size(); j++) {
+					//If this isn't the first set of lines, separate with a comma
+					String current_segment = "";
+					if(j > 0) {
+						current_segment += ", ";
+					}
+
+					//If the code block line numbers are the same, there's no need to say "line 45 to line 45"
+					if (lineNumbers.get(i).get(j).getKey() == lineNumbers.get(i).get(j).getValue()) {
+						current_segment += "line %2$d";
+						formatted_lines = String.format(current_segment, lineNumbers.get(i).get(j).getKey());
+					} else {
+						current_segment += "lines %2$d to %3$d";
+						formatted_lines = String.format(current_segment, lineNumbers.get(i).get(j).getKey(), lineNumbers.get(i).get(j).getValue());
+					}
+				}
+				String formatted_final = formatted_filename + formatted_lines;
+				stringJoiner.add(formatted_final);
 			}
 
 			//Get the joined string and add it to the report.
@@ -82,7 +76,7 @@ public class ReportGenerator implements IReportGenerator {
 
 			//Assemble the information into a FileReportItem and add it to the report.
 			float score = codeBlockGroup.getCodeBlock(sourceFile).getBlockScore();
-			FileReportItem reportItem = new FileReportItem(legacyDetectionType, score, codeBlockGroup.getCodeBlocks(), lineNumbers, joinedString);
+			FileReportItem reportItem = new FileReportItem(detectionType.getIdentifier(), score, codeBlockGroup.getCodeBlocks(), joinedString);
 			fileReport.AddReportItem(reportItem);
 		}
 
