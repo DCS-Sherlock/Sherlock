4c66122c614df69307db950efa07f0cded7e57d3
James <james.mt13@gmail.com>
2019-03-17 15:27:10 +0000

    fix latest

--- src/main/java/uk/ac/warwick/dcs/sherlock/engine/storage/base/EntityJob.java
+++ src/main/java/uk/ac/warwick/dcs/sherlock/engine/storage/base/EntityJob.java
@@ -97,6 +97,7 @@ public class EntityJob implements IJob, Serializable {
 
 	@Override
 	public IResultJob getLatestResult() {
+		BaseStorage.instance.database.refreshObject(this);
 		return this.results.size() > 0 ? this.results.get(this.results.size() - 1) : null;
 	}
 
