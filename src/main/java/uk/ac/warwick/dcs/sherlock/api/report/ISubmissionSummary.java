c1b29e61a08220672cd9a7efc94ee0dc1e53157e
Matthew <matthewfox413@gmail.com>
2019-04-30 00:47:23 +0100

    ISubmissionSummary docs

--- src/main/java/uk/ac/warwick/dcs/sherlock/api/report/ISubmissionSummary.java
+++ src/main/java/uk/ac/warwick/dcs/sherlock/api/report/ISubmissionSummary.java
@@ -6,9 +6,24 @@ import java.util.*;
 
 public interface ISubmissionSummary {
 
+	/**
+	 * Retrieves the submission id.
+	 *
+	 * @return the persistent id of this submission.
+	 */
 	long getPersistentId();
 
+	/**
+	 * Retrieves the score.
+	 *
+	 * @return the overall score for this submission.
+	 */
 	float getScore();
 
+	/**
+	 * Retrieves the matching submission list.
+	 *
+	 * @return the list of matching submissions and their relative scores.
+	 */
 	List<ITuple<Long, Float>> getMatchingSubmissions();
 }
