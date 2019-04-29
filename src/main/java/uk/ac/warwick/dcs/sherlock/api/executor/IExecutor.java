374c70f5f35b54a97115e26e16fa3102cb5658d1
James <james.mt13@gmail.com>
2019-04-30 00:23:15 +0100

    document api.executor

--- src/main/java/uk/ac/warwick/dcs/sherlock/api/executor/IExecutor.java
+++ src/main/java/uk/ac/warwick/dcs/sherlock/api/executor/IExecutor.java
@@ -4,26 +4,75 @@ import uk.ac.warwick.dcs.sherlock.api.component.IJob;
 
 import java.util.*;
 
+/**
+ * Execution Module interface, defines an execution scheme
+ */
 public interface IExecutor {
 
+	/**
+	 * Returns a list of all active, queued or recently finished job statuses
+	 * @return list of statuses
+	 */
 	List<IJobStatus> getAllJobStatuses();
 
+	/**
+	 * Returns a list of queued jobs
+	 * @return list of jobs
+	 */
 	List<IJob> getWaitingJobs();
 
+	/**
+	 * Gets the job status for a specific job
+	 * @param job job to get status of
+	 * @return status
+	 */
 	IJobStatus getJobStatus(IJob job);
 
+	/**
+	 * Gets the job from a job status
+	 * @param jobStatus job status instance
+	 * @return corresponding job
+	 */
 	IJob getJob(IJobStatus jobStatus);
 
+	/**
+	 * shutsdown the executor
+	 */
 	void shutdown();
 
+	/**
+	 * submits a job to this executor
+	 * @param job job to submit
+	 * @return was successful?
+	 */
 	boolean submitJob(IJob job);
 
+	/**
+	 * removes a finished job from the list of statuses
+	 * @param jobStatus status of finished job to remove from list
+	 * @return successful
+	 */
 	boolean dismissJob(IJobStatus jobStatus);
 
+	/**
+	 * removes a finished job from the list of statuses
+	 * @param job job instance of finished job to remove from list
+	 * @return successful
+	 */
 	boolean dismissJob(IJob job);
 
+	/**
+	 * Cancels the execution of a job
+	 * @param jobStatus job status of job to cancel
+	 * @return successful
+	 */
 	boolean cancelJob(IJobStatus jobStatus);
 
+	/**
+	 * Cancels the execution of a job
+	 * @param job job instance to cancel
+	 * @return successful
+	 */
 	boolean cancelJob(IJob job);
 
 }
--- src/main/java/uk/ac/warwick/dcs/sherlock/api/executor/IJobStatus.java
+++ src/main/java/uk/ac/warwick/dcs/sherlock/api/executor/IJobStatus.java
@@ -4,26 +4,77 @@ import javax.validation.constraints.NotNull;
 import java.time.Duration;
 import java.time.Instant;
 
+/**
+ * Interface defining the status information available for a job
+ */
 public interface IJobStatus extends Comparable<IJobStatus> {
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	int compareTo(@NotNull IJobStatus o);
 
+	/**
+	 * fetches the current duration of the job, if job finished gives the total runtime
+	 *
+	 * @return duration
+	 */
 	Duration getDuration();
 
+	/**
+	 * Fetches the current duration of the job as a formatted string, if job finished gives the total runtime
+	 *
+	 * @return duration as string
+	 */
 	String getFormattedDuration();
 
+	/**
+	 * get the id of the jobstatus
+	 *
+	 * @return id
+	 */
 	int getId();
 
+	/**
+	 * Returns the current status message for the job
+	 *
+	 * @return message
+	 */
 	String getMessage();
 
+	/**
+	 * returns the current progress estimate for the job, between 0 and 1
+	 *
+	 * @return progress estimate
+	 */
 	float getProgress();
 
+	/**
+	 * returns the current progress estimate for the job, as percentage between 0 and 100
+	 *
+	 * @return progress estimate percentage
+	 */
 	int getProgressInt();
 
+	/**
+	 * returns the start time of the job
+	 *
+	 * @return start time
+	 */
 	Instant getStartTime();
 
+	/**
+	 * Fetches the current "step" for the job, the stage it is at
+	 *
+	 * @return stage of job
+	 */
 	int getStep();
 
+	/**
+	 * returns whether the job has finished
+	 *
+	 * @return finished
+	 */
 	boolean isFinished();
 }
